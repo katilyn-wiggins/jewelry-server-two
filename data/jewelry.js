698273e54cbcc011cd0d3925ff57de6413ff2505
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-02-19 21:12:29 -0300

    added placekittens

--- data/jewelry.js
+++ data/jewelry.js
@@ -1,6 +1,6 @@
 module.exports=[
   {
-    image: 'y.png',
+    image: url('http://placekitten.com/g/200/300'),
     name: 'ember',
     description: 'antique teacup piece set in .925 silver, silver chain included',
     price: 60,
@@ -8,7 +8,7 @@ module.exports=[
     made_of_silver: true
   },
   {
-    image: 'y.png',
+    image: url('http://placekitten.com/g/200/300'),
     name: 'imelda',
     description: 'antique teacup piece set in .925 silver, silver chain included',
     price: 60,
@@ -16,7 +16,7 @@ module.exports=[
     made_of_silver: true
   },
   {
-    image: 'y.png',
+    image: url('http://placekitten.com/g/200/300'),
     name: 'constantine',
     description: 'turquoise set in sterling silver on silver band',
     price: 75,
@@ -24,7 +24,7 @@ module.exports=[
     made_of_silver: true
   },
   {
-    image: 'y.png',
+    image: url('http://placekitten.com/g/200/300'),
     name: 'mary',
     description: 'antique teacup peice set in .925 silver, silver chain included',
     price: 60,
@@ -32,7 +32,7 @@ module.exports=[
     made_of_silver: true
   },
   {
-    image: 'y.png',
+    image: url('http://placekitten.com/g/200/300'),
     name: 'esther',
     description: 'antique teacup piece set in .925 silver, silver chain included',
     price: 60,
@@ -40,7 +40,7 @@ module.exports=[
     made_of_silver: true
   },
   {
-    image: 'y.png',
+    image: url('http://placekitten.com/g/200/300'),
     name: 'dianna',
     description: 'antique teacup peice set in .925 silver, silver chain included',
     price: 60,
